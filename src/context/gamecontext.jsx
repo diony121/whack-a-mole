762a374e6fc7429a12a4808a29fad3c9552237eb
Dionicio <floresdionicio681@gmail.com>
2026-07-05 23:25:10 -0600

    Context for the game is declared

---
+++ src/context/gamecontext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useContext } from "react";
+
+const GameContext = createContext();
+
+export const GameProvider = ({ Children}) => {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState([]);
+  const [gameState, setGameState] = useState("welcome");
+ 
+  const startGame = () => {
+    setScore(0);
+    setGameState("playing");
+  };
+
+  const value = {
+    score,
+    setScore,
+    highScore,
+    setHighScore,
+    gameState,
+    setGameState,
+    startGame,
+  };
+
+  return (
+    <GameContext.Provider value={value}>
+      {Children}
+    </GameContext.Provider>
+  );
+};
+
+export const useGame = () => useContext(GameContext);
